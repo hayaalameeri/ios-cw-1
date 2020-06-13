9d7b2fafd3b6d0213d64472962b118f6bb6afeef
hayaalameeri <hayoony.m@gmail.com>
2020-06-13 19:47:01 +0300

    cw completed

--- CW-1-Haya660.playground/Contents.swift
+++ CW-1-Haya660.playground/Contents.swift
@@ -1,5 +1,36 @@
 import UIKit
 
 
-let name = "haya"
+let name = "Haya"
 
+var height = 171
+var age = 17
+var hobby1 = "writing"
+var hobby2 = "baking"
+
+print("My name is \(name) and my height is \(height)cm. I am \(age) years old and my hobbies are \(hobby1) and \(hobby2). " )
+var friendname = "hessa"
+var friendheight = 165
+var friendage = 18
+var friendhobby1 = "painting"
+var friendhobby2 = "sewing"
+
+let age_difference = friendage - age
+name == friendname
+name != friendname
+friendheight <= height
+friendheight == height
+friendheight >= height
+friendheight != height
+age == friendage
+age <= friendage
+age >= friendage
+age != friendage
+hobby1 == friendhobby1
+hobby1 != friendhobby1
+hobby2 == friendhobby2
+hobby2 != friendhobby2
+hobby1 == friendhobby2
+hobby1 != friendhobby2
+hobby2 == friendhobby1
+hobby2 != friendhobby1
